facd3df4aea83298709dea9af2ab208554c36e5f
aws755 <skypeskype755@gmail.com>
2025-08-05 12:09:35 +1000

    terraform vpc module

---
+++ outputs.tf
@@ -0,0 +1,30 @@
+# output "subnets_info" {
+#   value = [
+#     {
+#       arn               = "arn:aws:ec2:us-east-1:060795933447:subnet/subnet-0e536e3e0b9b9f494"
+#       availability_zone = "us-east-1a"
+#       cidr_block        = "10.0.1.0/24"
+#       id                = "subnet-0e536e3e0b9b9f494"
+#       vpc_id            = "vpc-0df4eefab1621120b"
+#       tags              = {
+#         Name         = "expense-dev-public-us-east-1a"
+#         Terraform    = "true"
+#         environment  = "dev"
+#         project_name = "expense"
+#       }
+#     },
+#     {
+#       arn               = "arn:aws:ec2:us-east-1:060795933447:subnet/subnet-0341f9842242d9b62"
+#       availability_zone = "us-east-1b"
+#       cidr_block        = "10.0.2.0/24"
+#       id                = "subnet-0341f9842242d9b62"
+#       vpc_id            = "vpc-0df4eefab1621120b"
+#       tags              = {
+#         Name         = "expense-dev-public-us-east-1b"
+#         Terraform    = "true"
+#         environment  = "dev"
+#         project_name = "expense"
+#       }
+#     }
+#   ]
+# }
--- variables.tf
+++ variables.tf
@@ -12,10 +12,81 @@ variable "project_name" {
 }
 
 variable "dns_hostnames" {
-    default = true
+  default = true
 }
 #optional
 variable "common_tags" {
-    type = map
+  type    = map(any)
+  default = {}
+}
+
+variable "igw_tags" {
+  default = {}
+}
+
+variable "vpc_tags" {
+  default = {}
+}
+
+variable "public_subnet_cidrs" {
+    type = list
+validation {
+    condition     = length(var.public_subnet_cidrs) == 2
+    error_message = "Please provide 2 valid public subnet cidrs"
+  }
+
+}
+
+variable "public_subnet_tags" {
+    default = {}
+}
+
+variable "private_subnet_cidrs" {
+    type = list
+validation {
+    condition     = length(var.private_subnet_cidrs) == 2
+    error_message = "Please provide 2 valid private subnet cidrs"
+  }
+
+}
+
+variable "private_subnet_tags" {
+    default = {}
+}
+
+variable "database_subnet_cidrs" {
+    type = list
+validation {
+    condition     = length(var.database_subnet_cidrs) == 2
+    error_message = "Please provide 2 valid database subnet cidrs"
+  }
+
+}
+
+variable "database_subnet_tags" {
+    default = {}
+}
+
+variable "nat_gateway_tags" {
+    default = {}
+}
+
+variable "public_route_table_tags" {
+    default = {}
+}
+
+variable "private_route_table_tags" {
+    default = {}
+}
+
+variable "database_route_table_tags" {
+    default = {}
+}
+
+variable "is_peering_required" {
+    default = false
+}
+
+variable "vpc_peering_tags" {
     default = {}
 }
